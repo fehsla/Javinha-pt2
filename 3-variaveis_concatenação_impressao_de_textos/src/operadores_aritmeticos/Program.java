eea8b6e2a1035eb930bdb911d870a45b7ec04e5f
Fernando <159575977+fehsla@users.noreply.github.com>
2026-05-26 22:39:13 -0300

    Add files via upload

---
+++ 3-variaveis_concatenação_impressao_de_textos/src/operadores_aritmeticos/Program.java
@@ -0,0 +1,59 @@
+package operadores_aritmeticos;
+
+import java.util.Locale;
+
+public class Program {
+	//oi amor 
+	
+	public static void main() {
+		int y = 1;
+		int x = 2;
+		double z = 20.000;
+		String T = "Muay Thai";
+		String C = "Capoeira";
+		String M = "MMA";
+		String J = "Jiu Jitsu";
+		
+		System.out.printf("%.2f%n", z);
+		System.out.printf("%.4f%n", z);
+		Locale.setDefault(Locale.US);
+		System.out.printf("%.4f%n", z);
+
+		System.out.println(z);
+		System.out.print(y);
+		System.out.println("É bom demaisssss");
+		System.out.println("Olá mundo");
+		System.out.print("Começo é sempre chato >.<");
+		System.out.println("Acaba video pelo amor de Deussss");
+		System.out.println("RESULTADO = " + x + "METROS");
+		System.out.printf("QUERO DORMIR %s a %s a %s a %s", T,C,M,J);
+		
+		
+		
+		//AAAAAAAAAAAAAA QUERO DORMIR
+		//QUERO DORMIRRRRRRRRRRRRRRRRRRRRRRR
+		//3 MINUTINHOS PARA DORMIR
+		
+		
+		
+	//EXERCICIO
+		String preguica = "Computer";
+		String cansaco = "Office desk";
+		
+		int tristeza = 30;
+		int sono = 5290;
+		char dormir = 'F';
+		
+		double devendo = 2100.0;
+		double devendo2 = 650.50;
+		double slaoqtaescritonatela = 53.234567;
+		
+		//IMPRESSAO 
+		System.out.printf("Products %n Computer, which prince is $ %f %nOffice desk, which price is %f%n"
+				+ "to com preguiça de terminar depois eu faço", devendo, devendo2);
+		
+		
+		
+	}
+	
+}
