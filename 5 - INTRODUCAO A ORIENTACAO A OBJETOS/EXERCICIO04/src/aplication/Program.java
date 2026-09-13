a98cfc66c547818701680948631c229afc7d32a3
Fernando <159575977+fehsla@users.noreply.github.com>
2026-09-13 18:18:57 -0300

    Update Program.java

--- 5 - INTRODUCAO A ORIENTACAO A OBJETOS/EXERCICIO04/src/aplication/Program.java
+++ 5 - INTRODUCAO A ORIENTACAO A OBJETOS/EXERCICIO04/src/aplication/Program.java
@@ -6,10 +6,10 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Insira o valor do Dollar");
+		System.out.println("Insira o valor do Dolar");
 		calculosDolar.dolar = sc.nextDouble();
 		
-		System.out.println("Insira o valor que precisa em Dollar");
+		System.out.println("Insira o valor que precisa em Dolar");
 		calculosDolar.valorConversao = sc.nextDouble();
 		
 		System.out.printf("VALOR QUE PRECISA SER PAGO PARA ADQUIRI O DOLAR: %.2f%n", 
